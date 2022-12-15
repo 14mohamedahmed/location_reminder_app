2e1eb1cd0ab21e2c71795afafcfd8c57ac4d795e
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2022-12-15 20:48:16 +0200

    handle fake data source in unit test

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,24 +4,49 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+
+    private var shouldReturnError = false
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error(
+                "Error getting reminders"
+            )
+        }
+        reminders?.let { return Result.Success(it) }
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        var reminder = reminders?.find {
+            it.id == id
+        }
+        return when {
+            shouldReturnError -> {
+                Result.Error("Reminder not found!")
+            }
+            reminder != null -> {
+                Result.Success(reminder)
+            }
+            else -> {
+                Result.Error("Reminder not found!")
+            }
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
 
