6242ec81d49adb50b191c5b36e27e028d59c7e67
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2022-12-16 22:09:10 +0200

    android test -=> reminder list fragment test 1- test no data to display 2- test data is displayed 3- click in FABto navigate to save reminder

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,31 +1,136 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest :
+    AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+    private lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
+    private val reminder =
+        ReminderDTO("reminder", "Description1", "Location1", 30.151351165, 29.351531, "1")
+
+    /**
+     * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
+     * at this step we will initialize Koin related code to be able to use it in out testing.
+     */
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
+
+    // test navigation btn to create reminder activity
+    @Test
+    fun clickReminderFAB_navigateToSaveReminderFragment() {
+        // init reminderListFragment scenario
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        // handle nav controller from mockito
+        val navController = Mockito.mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+        // navigate to save reminder fragment
+        Espresso.onView(ViewMatchers.withId(R.id.addReminderFAB)).perform(ViewActions.click())
+        // verify navigation success
+        Mockito.verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
+    // check if the noDataTextView displayed if list is empty
+    @Test
+    fun onUI_noDataDisplayed(): Unit = runBlocking {
+        // save reminder
+        repository.saveReminder(reminder)
+        // delete all reminders
+        repository.deleteAllReminders()
+        // launch reminder list fragment
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        // check if list is empty display no data
+        Espresso.onView(ViewMatchers.withId(R.id.noDataTextView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    // test if the reminder displayed in recycler adapter after saved
+    @Test
+    fun remindersList_DisplayedInUI(): Unit = runBlocking {
+        // save reminder
+        repository.saveReminder(reminder)
+        // launch reminder list fragment
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        // check if add reminder is exist in recycler view
+        Espresso.onView(ViewMatchers.withId(R.id.reminderssRecyclerView)).perform(
+            RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+                ViewMatchers.hasDescendant(ViewMatchers.withText(reminder.title))
+            )
+        )
+    }
+
+
 }
