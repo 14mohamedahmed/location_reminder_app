5924e3d9d7ff6cd19bded81601070412ef1b08f9
Mohamed Ahmed <mohamed.ahmed@8worx.com>
2022-12-17 22:49:16 +0200

    check for android version before set Intent flag because from API 31 and above must be MUTABLE or IMMUTABLE flag

--- app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
+++ app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
@@ -15,18 +15,17 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 private const val NOTIFICATION_CHANNEL_ID = BuildConfig.APPLICATION_ID + ".channel"
 
 fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
-    val notificationManager = context
-        .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     // We need to create a NotificationChannel associated with our CHANNEL_ID before sending a notification.
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-        && notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && notificationManager.getNotificationChannel(
+            NOTIFICATION_CHANNEL_ID
+        ) == null
     ) {
         val name = context.getString(R.string.app_name)
         val channel = NotificationChannel(
-            NOTIFICATION_CHANNEL_ID,
-            name,
-            NotificationManager.IMPORTANCE_DEFAULT
+            NOTIFICATION_CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT
         )
         notificationManager.createNotificationChannel(channel)
     }
@@ -34,20 +33,21 @@ fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
     val intent = ReminderDescriptionActivity.newIntent(context.applicationContext, reminderDataItem)
 
     //create a pending intent that opens ReminderDescriptionActivity when the user clicks on the notification
-    val stackBuilder = TaskStackBuilder.create(context)
-        .addParentStack(ReminderDescriptionActivity::class.java)
-        .addNextIntent(intent)
-    val notificationPendingIntent = stackBuilder
-        .getPendingIntent(getUniqueId(), PendingIntent.FLAG_UPDATE_CURRENT)
+    val stackBuilder =
+        TaskStackBuilder.create(context).addParentStack(ReminderDescriptionActivity::class.java)
+            .addNextIntent(intent)
+    val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        PendingIntent.FLAG_IMMUTABLE
+    } else {
+        PendingIntent.FLAG_UPDATE_CURRENT
+    }
+    val notificationPendingIntent = stackBuilder.getPendingIntent(getUniqueId(), flag)
 
 //    build the notification object with the data to be shown
     val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-        .setSmallIcon(R.mipmap.ic_launcher)
-        .setContentTitle(reminderDataItem.title)
-        .setContentText(reminderDataItem.location)
-        .setContentIntent(notificationPendingIntent)
-        .setAutoCancel(true)
-        .build()
+        .setSmallIcon(R.mipmap.ic_launcher).setContentTitle(reminderDataItem.title)
+        .setContentText(reminderDataItem.location).setContentIntent(notificationPendingIntent)
+        .setAutoCancel(true).build()
 
     notificationManager.notify(getUniqueId(), notification)
 }
